9c39a3eb3839dc330a0a6f27b0f1b8dcb9e0e2af
bjorn3 <bjorn3@users.noreply.github.com>
2019-02-19 17:15:03 +0100

    [WIP]

--- test.sh
+++ test.sh
@@ -35,17 +35,8 @@ $RUSTC --sysroot ./build_sysroot/sysroot example/mod_bench.rs --crate-type bin
 #echo "[BUILD] sysroot in release mode"
 #./build_sysroot/build_sysroot.sh --release
 
-COMPILE_MOD_BENCH_INLINE="$RUSTC --sysroot ./build_sysroot/sysroot example/mod_bench.rs --crate-type bin -Zmir-opt-level=3 -O --crate-name mod_bench_inline"
-COMPILE_MOD_BENCH_LLVM_0="rustc example/mod_bench.rs --crate-type bin -Copt-level=0 -o target/out/mod_bench_llvm_0 -Cpanic=abort"
-COMPILE_MOD_BENCH_LLVM_1="rustc example/mod_bench.rs --crate-type bin -Copt-level=1 -o target/out/mod_bench_llvm_1 -Cpanic=abort"
-COMPILE_MOD_BENCH_LLVM_2="rustc example/mod_bench.rs --crate-type bin -Copt-level=2 -o target/out/mod_bench_llvm_2 -Cpanic=abort"
-COMPILE_MOD_BENCH_LLVM_3="rustc example/mod_bench.rs --crate-type bin -Copt-level=3 -o target/out/mod_bench_llvm_3 -Cpanic=abort"
-
-# Use 100 runs, because a single compilations doesn't take more than ~150ms, so it isn't very slow
-hyperfine --runs 100 "$COMPILE_MOD_BENCH_INLINE" "$COMPILE_MOD_BENCH_LLVM_0" "$COMPILE_MOD_BENCH_LLVM_1" "$COMPILE_MOD_BENCH_LLVM_2" "$COMPILE_MOD_BENCH_LLVM_3"
-
-echo
-echo "[Bench] mod_bench"
-hyperfine ./target/out/mod_bench{,_inline} ./target/out/mod_bench_llvm_*
+git clone https://github.com/rust-lang/rust.git
+cd rust/src/tools/compiletest/
+cargo run -- --rustc-path=$(whereis rustc) --lldb-python=python --docck-python=python --src-base=$(pwd)/../../test --build-base=/tmp --stage-id=stage1 --mode=run-pass --cc=gcc --cxx=g++ --cflags=""
 
 cat target/out/log.txt | sort | uniq -c
