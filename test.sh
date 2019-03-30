c6d27e89daebfbc167ea8a338136ad225d1e5968
bjorn3 <bjorn3@users.noreply.github.com>
2019-03-30 18:38:12 +0100

    Rustup patch

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ rm -r target/out || true
 mkdir -p target/out/clif
 
 echo "[BUILD] mini_core"
-$RUSTC example/mini_core.rs --crate-name mini_core --crate-type dylib
+$RUSTC example/mini_core.rs --crate-name mini_core --crate-type lib
 
 echo "[BUILD] example"
 $RUSTC example/example.rs --crate-type lib
@@ -67,7 +67,7 @@ index b75e312d..aef8bc14 100644
 -[submodule "src/llvm-project"]
 -	path = src/llvm-project
 -	url = https://github.com/rust-lang/llvm-project.git
--	branch = rustc/8.0-2019-01-16
+-	branch = rustc/8.0-2019-03-18
  [submodule "src/doc/embedded-book"]
  	path = src/doc/embedded-book
  	url = https://github.com/rust-embedded/book.git
@@ -186,7 +186,7 @@ cat > config.toml <<EOF
 codegen-backends = []
 [build]
 local-rebuild = true
-rustc = "$HOME/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/bin/rustc"
+rustc = "$HOME/.rustup/toolchains/nightly-x86_64-apple-darwin/bin/rustc"
 EOF
 
 rm -r src/test/run-pass/{asm-*,abi-*,extern/,panic-runtime/,panics/,unsized-locals/,proc-macro/,threads-sendsync/,thinlto/,simd/} || true
